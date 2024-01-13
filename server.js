b44e918e3f87b0441ca4b077b19d7502d1404444
marliemarl <marlieahue@gmail.com>
2024-01-13 13:11:17 -0500

    new update mongodb

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const PORT = 3000;
+require('dotenv').config({ path: '.env' })
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
@@ -17,6 +20,22 @@ app.get('/', function(req, res){
 app.post('/users', (req, res) => {
 	console.log(req.body);
 })
+
+MongoClient.connect('process.env.MONGO_URI')
+    .then(client => {
+        const db = client.db('practice');
+        const usersCollection = db.collection('users');
+
+        app.post('/users', (req, res) => {
+            usersCollection
+        .insertOne(req.body)
+        .then(result => {
+            res.redirect('/');
+        })
+        .catch(error => console.log(error))
+  })
+})
+    .catch(error => console.error(error))
 
 app.listen(PORT, function() {
     console.log(`Server is live! Listening at port ${PORT}`);
