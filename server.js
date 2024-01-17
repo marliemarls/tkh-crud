410d7ea5cc72f8945e435c3916996f39ba419e8c
marliemarl <marlieahue@gmail.com>
2024-01-16 20:27:39 -0500

    server update

--- server.js
+++ server.js
@@ -12,6 +12,32 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
+MongoClient.connect(process.env.MONGO_URI)
+    .then(client => {
+        const db = client.db('practice');
+        const usersCollection = db.collection('users');
+
+        app.get('/', (req, res) => {
+            usersCollection
+                .find()
+                .toArray()
+                .then(results => {
+                    res.render('index.ejs', { usersCollection: results })
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.post('/users', (req, res) => {
+        usersCollection
+        .insertOne(req.body)
+        .then(res => {
+            res.redirect('/');
+        })
+        .catch(error => console.log(error))
+  })
+})
+.catch(error => console.error(error))
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
     //if this doesnt work remember to save file after
@@ -21,21 +47,7 @@ app.post('/users', (req, res) => {
 	console.log(req.body);
 })
 
-MongoClient.connect('process.env.MONGO_URI')
-    .then(client => {
-        const db = client.db('practice');
-        const usersCollection = db.collection('users');
 
-        app.post('/users', (req, res) => {
-            usersCollection
-        .insertOne(req.body)
-        .then(result => {
-            res.redirect('/');
-        })
-        .catch(error => console.log(error))
-  })
-})
-    .catch(error => console.error(error))
 
 app.listen(PORT, function() {
     console.log(`Server is live! Listening at port ${PORT}`);
