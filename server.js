7246cb6e57cfa56deaa67e7baa45551ec450124d
marliemarl <marlieahue@gmail.com>
2024-01-19 23:06:42 -0500

    reupdate for server

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// console.log("is this thing on?")
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
@@ -7,41 +5,38 @@ const app = express();
 const PORT = 3000;
 require('dotenv').config({ path: '.env' })
 
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 MongoClient.connect(process.env.MONGO_URI)
-    .then(client => {
-        const db = client.db('practice');
-        const usersCollection = db.collection('users');
+.then(client => {
+	const db = client.db('practice');
+	const usersCollection = db.collection('users');
 
-        app.get('/', (req, res) => {
-            usersCollection
-                .find()
-                .toArray()
-                .then(results => {
-                    res.render('index.ejs', { usersCollection: results })
-                })
-                .catch(error => console.error(error))
-        })
+	app.get('/', (req, res) => {
+		usersCollection
+			.find()
+			.toArray()
+			.then(results => {
+				res.render('index.ejs', { usersCollection: results })
+			})
+			.catch(error => console.error(error))
+	})
 
-        app.post('/users', (req, res) => {
-        usersCollection
+	app.post('/users', (req, res) => {
+		usersCollection
         .insertOne(req.body)
         .then(result => {
             res.redirect('/');
         })
         .catch(error => console.log(error))
-  })
+	 })
 })
 .catch(error => console.error(error))
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-    //if this doesnt work remember to save file after
-})
 
 app.post('/users', (req, res) => {
 	console.log(req.body);
@@ -74,19 +69,16 @@ app.put('/users', (req, res) => {
 app.delete('/users', (req, res) => {
     usersCollection
         .deleteOne(
-            { username: req.body.username }
+        { username: req.body.username }
         )
         .then(result => {
-            console.log(`Deleted ${req.body.username}`)
-            console.log(result);
+        console.log(`Deleted ${req.body.username}`)
+        console.log(result);
             res.json('Deleted user')
         })
         .catch(error => console.error(error))
-})
-
+}) 
 
 app.listen(PORT, function() {
-    console.log(`Server is live! Listening at port ${PORT}`);
-
+	console.log(`Server is live! Listening at port ${PORT}`);
 })
-
